588a4a0d104a298213814550faf5e066ec64a510
barriospablo <exepablo96@gmail.com>
2020-12-15 21:25:00 -0300

    unused reducer deleted

--- index.js
+++ index.js
@@ -3,31 +3,9 @@ const { createStore, combineReducers } = Redux;
 // Actions
 const BUY_XBOX = "BUY_XBOX";
 const RETURN_XBOX = "RETURN_XBOX";
-const BUY_POKEMON = "BUY_POKEMON";
-const RETURN_POKEMON = "RETURN_POKEMON";
-const BUY_YOSHI = "BUY_YOSHI";
 const BUY_SOME = "BUY_SOME";
 const RETURN_SOME = "RETURN_SOME";
 
-const buyPokemon = (cant) => {
-  return {
-    type: BUY_POKEMON,
-    payload: cant,
-  };
-};
-
-const returnPokemon = (cant) => {
-  return {
-    type: RETURN_POKEMON,
-    payload: cant,
-  };
-};
-const buyYoshi = (cant) => {
-  return {
-    type: BUY_YOSHI,
-    payload: cant,
-  };
-};
 const buyConsole = (cant) => {
   return {
     type: BUY_XBOX,
@@ -55,35 +33,6 @@ const returnSome = (cant) => {
 };
 
 // Reducers
-const initialState = {
-  pokemon: 10,
-  yoshi: 10,
-};
-
-const gamesReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case BUY_POKEMON: {
-      return {
-        ...state,
-        pokemon: state.pokemon - action.payload,
-      };
-    }
-    case RETURN_POKEMON: {
-      return {
-        ...state,
-        pokemon: state.pokemon + action.payload,
-      };
-    }
-    case BUY_YOSHI: {
-      return {
-        ...state,
-        yoshi: state.yoshi - action.payload,
-      };
-    }
-    default:
-      return state;
-  }
-};
 
 const defaultConsoles = {
   xbox: 30,
@@ -128,19 +77,11 @@ const inventoryReducer = (state = inventory, action) => {
   }
 };
 const rootReducres = combineReducers({
-  gamesReducer,
   consoleReducer,
   inventoryReducer,
 });
 // Store
 const store = Redux.createStore(rootReducres);
-// console.log("Estado inicial:", store.getState());
-// store.subscribe(() => {
-//   console.log("Cambio de estado:", store.getState());
-// });
-// store.dispatch(buyPokemon(3));
-// store.dispatch(returnPokemon(2));
-// store.dispatch(buyConsole(2));
 const inventoryQuantity = document.getElementById("quantity");
 const valueSpam = document.getElementById("value");
 function render() {
